df29b7a8d6966eff1e2b9e53b47e1261b5fe25cb
SirPotatoIV <otoolej33@gmail.com>
2019-12-29 22:14:37 -0600

    added basic form to html and tested that enter button worked for submitting.

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,18 @@
 function mainFunction(){
     // console.log("Gotta Catch'em All.")
-    axios.get('https://pokeapi.co/api/v2/pokemon/squirtle')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
+    
+    function requestApi(){
+        axios.get('https://pokeapi.co/api/v2/pokemon/squirtle')
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+    }
+    requestApi()
 
 }
 mainFunction()
