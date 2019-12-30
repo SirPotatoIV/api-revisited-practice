f3081db228cd99da01d607e1ea349fb8d048a4b3
SirPotatoIV <otoolej33@gmail.com>
2019-12-29 21:52:19 -0600

    successfully performed a get request from the poke api.

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,14 @@
 function mainFunction(){
-    console.log("Gotta Catch'em All.")
+    // console.log("Gotta Catch'em All.")
+    axios.get('https://pokeapi.co/api/v2/pokemon/squirtle')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+
 }
 mainFunction()
