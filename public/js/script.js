88a9a5eee78916d46444e8a3433badc23d656751
SirPotatoIV <otoolej33@gmail.com>
2019-12-30 00:18:33 -0600

    Added code to get four random pokemon names to use to guess and use the first random name as the correct guess name

--- public/js/script.js
+++ public/js/script.js
@@ -13,40 +13,47 @@ function mainFunction(){
     }
     search()
 
-    function getRandomPokemon(){
-        const randomId = Math.floor(Math.random()*150)
-        // console.log(randomId)
-             // https://pokeapi.co/
-             axios.get(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
-             .then(function ({data}) {
-                // handle success
-                // console.log(data);
-                // displaySearchPokemon(data.sprites.front_default)
-                const pokePicEl = document.getElementById("mysteryPokePicDiv");
-                pokePicEl.innerHTML= `<img id="mysteryPokemon" src="${data.sprites.front_default}"/>`
-             })
-             .catch(function (error) {
-                 // handle error
-                 console.log(error);
-             })
+    function getGuessNames(randomIds){
+        const pokemonNamesList = [];
+        const pokemonGuessNames = [];
+        // const randomPokemon 
+        axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=150`)
+        .then(function ({data}) {
+            // handle success
+            // store all pokemon names
+            for(let i=0; i < data.results.length; i++){
+                pokemonNamesList.push(data.results[i].name)
+            }
+            // get random names for guessing
+            for(let i=0; i < 4; i++){
+                const randomId = Math.floor(Math.random()*150);
+                const pokemonName = pokemonNamesList[randomId];
+                pokemonGuessNames.push(pokemonName)
+            }
+            // get pokemon to guess image
+            getRandomPokemon(pokemonGuessNames)
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+    }
+    getGuessNames()
+    
+    function getRandomPokemon(pokemonGuessNames){
+        // https://pokeapi.co/
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonGuessNames[0]}`)
+        .then(function ({data}) {
+            // handle success
+            const pokePicEl = document.getElementById("mysteryPokePicDiv");
+            pokePicEl.innerHTML= `<img id="mysteryPokemon" src="${data.sprites.front_default}"/>`
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
     }
     // getRandomPokemon()
-
-    function getAllPokemon(){
-        const pokemonNameList = [];
-        axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=150`)
-             .then(function ({data}) {
-                // handle success
-                for(let i=0; i < data.results.length; i++){
-                    pokemonNameList.push(data.results[i].name)
-                }
-             })
-             .catch(function (error) {
-                 // handle error
-                 console.log(error);
-             })
-    }
-    getAllPokemon()
 
     function requestApiSearch(searchText){
         // https://pokeapi.co/
