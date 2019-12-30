621c17ffc22407e04e9766c8c0e8d7ab45f0361a
SirPotatoIV <otoolej33@gmail.com>
2019-12-29 23:39:41 -0600

    Added new section to html for where the pokemon guessing game will be. Successfully got a random pokemon to display and have the image be all black.

--- public/js/script.js
+++ public/js/script.js
@@ -8,33 +8,47 @@ function mainFunction(){
         searchBtnEl.addEventListener("click", function(){
             event.preventDefault();
             const searchText = searchInputEl.value;
-            requestApi(searchText);
-            // console.log(searchInputEl.value)
-
+            requestApiSearch(searchText);
         })
     }
     search()
 
-    function requestApi(searchText){
+    function getRandomPokemon(){
+        const randomId = Math.floor(Math.random()*150)
+        // console.log(randomId)
+             // https://pokeapi.co/
+             axios.get(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
+             .then(function ({data}) {
+                // handle success
+                // console.log(data);
+                // displaySearchPokemon(data.sprites.front_default)
+                const pokePicEl = document.getElementById("mysteryPokePicDiv");
+                pokePicEl.innerHTML= `<img id="mysteryPokemon" src="${data.sprites.front_default}"/>`
+             })
+             .catch(function (error) {
+                 // handle error
+                 console.log(error);
+             })
+    }
+    getRandomPokemon()
+
+    function requestApiSearch(searchText){
         // https://pokeapi.co/
         axios.get(`https://pokeapi.co/api/v2/pokemon/${searchText}`)
             .then(function ({data}) {
                 // handle success
                 // console.log(data);
-                displayPokemon(data.sprites.front_default)
+                displaySearchPokemon(data.sprites.front_default)
             })
             .catch(function (error) {
                 // handle error
                 console.log(error);
             })
     }
-    // requestApi()
 
-    function displayPokemon(pokemonImage){
+    function displaySearchPokemon(pokemonImage){
         const pokePicEl = document.getElementById("pokePic");
-        pokePicEl.innerHTML= `
-            <img src="${pokemonImage}"/>
-            <img id="mysteryPokemon" src="${pokemonImage}"/>`
+        pokePicEl.innerHTML= `<img src="${pokemonImage}"/>`
     }
 
 }
